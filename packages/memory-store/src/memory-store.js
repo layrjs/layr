d75c7ef4b8e11ebfc9e4fd46abf31cf0a88906c8
Manuel Vila <mvila@3base.com>
2019-03-10 11:20:09 +0900

    memory-store: Implement CRUD operations

--- packages/memory-store/src/memory-store.js
+++ packages/memory-store/src/memory-store.js
@@ -1,1 +1,93 @@
-export class MemoryStore {}
+export class MemoryStore {
+  _collections = {};
+
+  get({_type, _id}, {return: returnFields} = {}) {
+    validateType(_type);
+    validateId(_id);
+
+    const document = this._collections[_type]?.[_id];
+
+    if (document === undefined) {
+      return undefined;
+    }
+
+    if (returnFields === undefined) {
+      return {_type, _id, ...document};
+    }
+
+    const result = {};
+    for (const [name, value] of Object.entries(returnFields)) {
+      if (!value) {
+        continue;
+      }
+      if (name === '_type') {
+        result._type = _type;
+      } else if (name === '_id') {
+        result._id = _id;
+      } else {
+        const fieldValue = document[name];
+        if (fieldValue !== undefined) {
+          result[name] = fieldValue;
+        }
+      }
+    }
+    return result;
+  }
+
+  set({_type, _id, ...changes}) {
+    validateType(_type);
+    validateId(_id);
+
+    let collection = this._collections[_type];
+    if (collection === undefined) {
+      collection = {};
+      this._collections[_type] = collection;
+    }
+
+    let document = collection[_id];
+    if (document === undefined) {
+      document = {};
+      collection[_id] = document;
+    }
+
+    for (const [name, value] of Object.entries(changes)) {
+      if (value !== undefined) {
+        document[name] = value;
+      } else {
+        delete document[name];
+      }
+    }
+  }
+
+  delete({_type, _id}) {
+    validateType(_type);
+    validateId(_id);
+
+    const document = this._collections[_type]?.[_id];
+    if (document === undefined) {
+      return false;
+    }
+    delete this._collections[_type][_id];
+    return true;
+  }
+}
+
+function validateType(_type) {
+  if (typeof _type !== 'string') {
+    throw new Error(`'_type' must be a string (provided type: ${typeof _type}`);
+  }
+
+  if (_type === '') {
+    throw new Error(`'_type' cannot be empty`);
+  }
+}
+
+function validateId(_id) {
+  if (typeof _id !== 'string') {
+    throw new Error(`'_id' must be a string (provided type: ${typeof _id}`);
+  }
+
+  if (_id === '') {
+    throw new Error(`'_id' cannot be empty`);
+  }
+}
--- packages/memory-store/tests/src/index.test.js
+++ packages/memory-store/tests/src/index.test.js
@@ -10,11 +10,18 @@ describe('@superstore/memory-store', () => {
     // Read
     let movie = store.get({_type: 'Movie', _id: 'abc123'});
     expect(movie).toEqual({_type: 'Movie', _id: 'abc123', title: 'Inception', genre: 'action'});
+    movie = store.get({_type: 'Movie', _id: 'abc123'}, {return: {title: true}}); // Partial read
+    expect(movie).toEqual({title: 'Inception'});
+    movie = store.get({_type: 'Movie', _id: 'xyz123'}); // Missing document
+    expect(movie).toBeUndefined();
+    movie = store.get({_type: 'Person', _id: 'xyz123'}); // Missing collection
+    expect(movie).toBeUndefined();
 
     // Update
     store.set({_type: 'Movie', _id: 'abc123', title: 'The Matrix', genre: undefined});
     movie = store.get({_type: 'Movie', _id: 'abc123'});
     expect(movie).toEqual({_type: 'Movie', _id: 'abc123', title: 'The Matrix'});
+    expect(Object.keys(movie).includes('genre')).toBe(false); // 'genre' has been deleted
 
     // Remove
     let hasBeenDeleted = store.delete({_type: 'Movie', _id: 'abc123'});
@@ -25,15 +32,20 @@ describe('@superstore/memory-store', () => {
     expect(hasBeenDeleted).toBe(false);
   });
 
-  test('References', () => {
+  test.skip('References', () => {
     const store = new MemoryStore();
 
+    // Let's set both a 'Movie' and a 'Person'
     store.set({
       _type: 'Movie',
       _id: 'abc123',
       title: 'Inception',
-      director: {_type: 'Person', _ref: true, _id: 'xyz123', fullName: 'Christopher Nolan'}
+      director: {_type: 'Person', _id: 'xyz123', fullName: 'Christopher Nolan'}
     });
+
+    // The director can be fetched from 'Person'
+    let person = store.get({_type: 'Person', _id: 'xyz123'});
+    expect(person).toEqual({_type: 'Person', _id: 'xyz123', fullName: 'Christopher Nolan'});
 
     // Will fetch both 'Movie' and 'Person'
     let movie = store.get({_type: 'Movie', _id: 'abc123'});
@@ -41,7 +53,7 @@ describe('@superstore/memory-store', () => {
       _type: 'Movie',
       _id: 'abc123',
       title: 'Inception',
-      director: {_type: 'Person', _ref: true, _id: 'xyz123', fullName: 'Christopher Nolan'}
+      director: {_type: 'Person', _id: 'xyz123', fullName: 'Christopher Nolan'}
     });
 
     // Will fetch 'Movie' only
@@ -54,5 +66,14 @@ describe('@superstore/memory-store', () => {
       title: 'Inception',
       director: {_id: 'xyz123'}
     });
+
+    // The director can be modified through its parent 'Movie'
+    store.set({
+      _type: 'Movie',
+      _id: 'abc123',
+      director: {_type: 'Person', _id: 'xyz123', fullName: 'C. Nolan'}
+    });
+    person = store.get({_type: 'Person', _id: 'xyz123'}, {return: {fullName: true}});
+    expect(person).toEqual({fullName: 'C. Nolan'});
   });
 });
