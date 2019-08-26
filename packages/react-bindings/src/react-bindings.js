87914ea6b0d24b9625c68e973c2e84ae80ef98a5
Manuel Vila <mvila@3base.com>
2019-08-26 14:34:47 +0900

    react-binding: Simplify @view() implementation

--- packages/react-bindings/src/react-bindings.js
+++ packages/react-bindings/src/react-bindings.js
@@ -22,34 +22,16 @@ export function useModel(model) {
 
 export function view() {
   return function (target, name, {value: component, configurable, enumerable}) {
-    // Credits: the 'autobind' logic comes from https://github.com/jayphelps/core-decorators/blob/6d9703e15414abdd093290697e85b663eb86455b/src/autobind.js#L48
-
     if (typeof component !== 'function') {
       throw new Error(`@view() can only be used on functions`);
     }
-
-    const {constructor} = target;
 
     return {
       configurable,
       enumerable,
       get() {
-        if (this === target) {
-          // Someone accesses the property directly on the prototype on which it is
-          // actually defined on
-          return component;
-        }
-
-        if (
-          this.constructor !== constructor &&
-          Object.getPrototypeOf(this).constructor === constructor
-        ) {
-          // Someone accesses the property directly on a prototype but it was found
-          // up the chain, not defined directly on it
-          return component;
-        }
-
         const model = this;
+
         const boundComponent = function (props) {
           if (isObservable(model)) {
             // eslint-disable-next-line react-hooks/rules-of-hooks
