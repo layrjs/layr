d0b3bfe1c4077c7608df4421f1c62038008f63e6
Manuel Vila <mvila@3base.com>
2019-08-31 08:06:55 +0900

    mongodb-store: Specify 'useUnifiedTopology' option to MongoClient

--- packages/mongodb-store/src/mongodb-store.js
+++ packages/mongodb-store/src/mongodb-store.js
@@ -281,7 +281,10 @@ export class MongoDBStore extends Registerable() {
   async _connectClient() {
     if (!this._client) {
       debug(`Connecting to MongoDB Server (connectionString: ${this._connectionString})...`);
-      this._client = await MongoClient.connect(this._connectionString, {useNewUrlParser: true});
+      this._client = await MongoClient.connect(this._connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
       debug(`Connected to MongoDB Server (connectionString: ${this._connectionString})`);
     }
   }
