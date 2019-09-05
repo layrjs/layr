3c7baef022c8d4c02314bf642606d054644a9282
Manuel Vila <mvila@3base.com>
2019-09-05 09:43:45 +0900

    document: Rename DocumentList's find() to load()

--- packages/document/src/document-list.js
+++ packages/document/src/document-list.js
@@ -2,7 +2,7 @@ import {ModelList} from '@liaison/model';
 
 export const DocumentList = documentModelName =>
   class DocumentList extends ModelList(documentModelName) {
-    async find(options) {
+    async load(options) {
       const Document = this.layer.get(documentModelName);
 
       this.items = await Document.find(options);
