2055cffbcce807f54f46a891fa4bccc7fa29157c
Manuel Vila <mvila@3base.com>
2019-09-09 07:24:15 +0900

    model: Remove ModelList

--- packages/model/src/index.js
+++ packages/model/src/index.js
@@ -2,6 +2,5 @@ export {Entity} from './entity';
 export {FieldMask} from './field-mask';
 export {Identity} from './identity';
 export {Model, field, isModel} from './model';
-export {ModelList} from './model-list';
 export {Singleton} from './singleton';
 export {validators, createValidator} from './validation';
--- packages/model/src/model-list.js
+++
@@ -1,6 +0,0 @@
-import {Model, field} from './model';
-
-export const ModelList = modelName =>
-  class ModelList extends Model {
-    @field(`${modelName}[]`) items;
-  };
