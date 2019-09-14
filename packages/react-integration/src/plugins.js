34f7018092cc0ef64ca1b6fe74cb2640391ec2e0
Manuel Vila <mvila@3base.com>
2019-09-14 18:32:52 +0900

    reat-integration: Implement Link's activeClassName and activeStyle options

--- packages/react-integration/src/plugins.js
+++ packages/react-integration/src/plugins.js
@@ -23,21 +23,36 @@ export function ReactRouterPlugin() {
         return router;
       },
 
-      Link(props) {
-        // eslint-disable-next-line react/prop-types
-        if (props.onClick) {
+      // eslint-disable-next-line react/prop-types
+      Link({href, onClick, className, style, activeClassName, activeStyle, ...props}) {
+        if (onClick) {
           throw new Error(`'onClick' attribute is not allowed`);
         }
 
         const handleClick = event => {
           if (!(event.shiftKey || event.ctrlKey || event.altKey || event.metaKey)) {
             event.preventDefault();
-            // eslint-disable-next-line react/prop-types
-            router.navigate(props.href);
+            router.navigate(href);
           }
         };
 
-        return <a {...props} onClick={handleClick} />;
+        const isActive = router.location.pathname === href;
+
+        if (isActive) {
+          if (activeClassName) {
+            const classes = className ? [className] : [];
+            classes.push(activeClassName);
+            className = classes.join(' ');
+          }
+
+          if (activeStyle) {
+            style = {...style, ...activeStyle};
+          }
+        }
+
+        return (
+          <a href={href} onClick={handleClick} className={className} style={style} {...props} />
+        );
       }
     });
 
