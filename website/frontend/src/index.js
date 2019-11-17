1374ce6dd96614592c5bbd0af5325c38224c9327
Manuel Vila <mvila@3base.com>
2019-11-18 08:01:57 +0900

    website: Allow Googlebot

--- website/frontend/src/index.js
+++ website/frontend/src/index.js
@@ -13,8 +13,12 @@ import {createLayer} from './layer';
   try {
     const layer = await createLayer();
     await layer.$open();
-    const {Root} = layer;
-    content = <Root.Main />;
+
+    if (process.env.NODE_ENV !== 'production') {
+      window.$layer = layer; // For debugging
+    }
+
+    content = <layer.Root.Main />;
   } catch (err) {
     console.error(err);
     content = <pre>{err.stack}</pre>;
