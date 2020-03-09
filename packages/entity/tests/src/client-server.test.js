90211307548d876fb009c24642c39d17ba521b1f
Manuel Vila <mvila@3base.com>
2020-03-09 15:51:11 +0900

    entity: Test client/server

---
+++ packages/entity/tests/src/client-server.test.js
@@ -0,0 +1,40 @@
+import {ComponentClient} from '@liaison/component-client';
+import {ComponentServer} from '@liaison/component-server';
+
+import {Entity, primaryIdentifier, attribute, expose} from '../../..';
+
+describe('Client-server', () => {
+  test('Simple entity', async () => {
+    const provider = function() {
+      class Counter extends Entity() {
+        @expose({get: true, set: true}) @primaryIdentifier() id;
+
+        @expose({get: true, set: true}) @attribute('number') value = 0;
+
+        @expose({call: true}) increment() {
+          this.value++;
+        }
+      }
+
+      return [Counter, Counter.prototype];
+    };
+
+    const server = new ComponentServer(provider);
+
+    const client = new ComponentClient(server);
+
+    const {Counter} = client.getComponents();
+
+    const counter = new Counter();
+
+    expect(counter.value).toBe(0);
+
+    counter.increment();
+
+    expect(counter.value).toBe(1);
+
+    counter.increment();
+
+    expect(counter.value).toBe(2);
+  });
+});
