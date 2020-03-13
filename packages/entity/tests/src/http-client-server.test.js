bbbf3854906be889d49b0fa8d881b536e24e653a
Manuel Vila <mvila@3base.com>
2020-03-13 13:48:31 +0900

    entity: Test HTTP client/server

---
+++ packages/entity/tests/src/http-client-server.test.js
@@ -0,0 +1,54 @@
+import {ComponentHTTPClient} from '@liaison/component-http-client';
+import {ComponentHTTPServer} from '@liaison/component-http-server';
+
+import {Entity, primaryIdentifier, attribute, expose} from '../../..';
+
+const SERVER_PORT = 4444;
+
+describe('HTTP client/server', () => {
+  let server;
+
+  beforeAll(async () => {
+    const provider = function() {
+      class Counter extends Entity {
+        @expose({get: true, set: true}) @primaryIdentifier() id;
+
+        @expose({get: true, set: true}) @attribute('number') value = 0;
+
+        @expose({call: true}) increment() {
+          this.value++;
+        }
+      }
+
+      return [Counter, Counter.prototype];
+    };
+
+    server = new ComponentHTTPServer(provider, {port: SERVER_PORT});
+
+    await server.start();
+  });
+
+  afterAll(async () => {
+    await server?.stop();
+  });
+
+  test('Simple entity', async () => {
+    const client = new ComponentHTTPClient(`http://localhost:${SERVER_PORT}`, {
+      baseComponents: [Entity]
+    });
+
+    const {Counter} = await client.getComponents();
+
+    const counter = new Counter();
+
+    expect(counter.value).toBe(0);
+
+    await counter.increment();
+
+    expect(counter.value).toBe(1);
+
+    await counter.increment();
+
+    expect(counter.value).toBe(2);
+  });
+});
