ab23212c1c83bd4cf433b970dbde78cf5e380622
Manuel Vila <mvila@3base.com>
2021-05-18 14:42:26 +0900

    Update dependencies

--- website/backend/src/components/application.ts
+++ website/backend/src/components/application.ts
@@ -1,4 +1,5 @@
 import {Component, provide} from '@layr/component';
+import {Routable} from '@layr/routable';
 
 import {Session} from './session';
 import {User} from './user';
@@ -6,7 +7,7 @@ import {Article} from './article';
 import {Newsletter} from './newsletter';
 import {JWT} from './jwt';
 
-export class Application extends Component {
+export class Application extends Routable(Component) {
   @provide() static Session = Session;
   @provide() static User = User;
   @provide() static Article = Article;
