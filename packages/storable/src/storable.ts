07c9f0d03e6ff6a1ebfa5f2068803acc2143c079
Manuel Vila <mvila@3base.com>
2021-05-29 16:33:13 +0900

    Update dependencies

--- packages/storable/src/storable.ts
+++ packages/storable/src/storable.ts
@@ -676,7 +676,7 @@ export function Storable<T extends Constructor<typeof Component>>(Base: T) {
       let storableHasBeenCreated = false;
 
       if (storable === undefined) {
-        storable = (await this.create(identifierDescriptor, {
+        storable = (await this.instantiate(identifierDescriptor, {
           isNew: false
         })) as InstanceType<T>;
         storableHasBeenCreated = true;
