4935352f94994c391ab597067e03151d3bf36aae
Manuel Vila <mvila@3base.com>
2023-01-08 08:48:40 +0900

    Edit "Hello, World!" app for Layr v2

--- examples/v2/hello-world-ts/backend/src/components/application.ts
+++ examples/v2/hello-world-ts/backend/src/components/application.ts
@@ -2,10 +2,10 @@ import {Component, method, expose} from '@layr/component';
 
 export class Application extends Component {
   @expose({call: true}) @method() static async getHelloWorld() {
-    const results = ['Hello, World!', 'Bonjour le monde !', 'こんにちは世界！'];
+    const translations = ['Hello, World!', 'Bonjour le monde !', 'こんにちは世界！'];
 
-    const result = results[Math.round(Math.random() * (results.length - 1))];
+    const translation = translations[Math.round(Math.random() * (translations.length - 1))];
 
-    return result;
+    return translation;
   }
 }
